b3bd936e24fbf6148b3dd72f0f097c329238bb78
AR-May <67507805+AR-May@users.noreply.github.com>
2020-10-01 13:03:09 +0200

    draft: add unit test for chmod post-action

---
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/ChmodPostActionTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Microsoft.TemplateEngine.Cli.UnitTests
+{
+    public class ChmodPostActionTests : EndToEndTestBase
+    {
+        [Theory(DisplayName = nameof(VerifyTemplatesWithChmodPostAction))]
+        [InlineData("TestAssets.TemplateWithChmodPostaction", "TemplateWithChmodPostaction.json")]
+        public void VerifyTemplatesWithChmodPostAction(string args, params string[] scripts)
+        {
+            Run(args, scripts);
+        }
+    }
+}
